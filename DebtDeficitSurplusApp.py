e77668378f0d7ffbdef96d36712177c9f1bfc2ea
JQTs369 <JQTs369@gmail.com>
2024-12-12 18:49:11 -0600

    Update DebtDeficitSurplusApp.py

--- DebtDeficitSurplusApp.py
+++ DebtDeficitSurplusApp.py
@@ -44,7 +44,7 @@ dfDeficit['Fiscal Year'] = dfDeficit['Fiscal Year'].astype(int)
 dfDeficit['Surplus or Deficit(-) Total'] = dfDeficit['Surplus or Deficit(-) Total'] * 1_000_000
 
 
-st.title("## Debt & Surplus/Deficit  \n### by POTUS or Year")
+st.title("Debt & Surplus/Deficit  \n# by President or Year")
 
 # User Selection Opitions
 viewType = st.selectbox('Select President Or Year by:', ['President','Year'])
