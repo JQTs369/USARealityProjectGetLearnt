bcc22dce5d2c95b6ddd417dee34d4805ca70ac48
JQTs369 <JQTs369@gmail.com>
2024-12-17 19:14:10 -0600

    Update DebtDeficitSurplusApp.py

--- DebtDeficitSurplusApp.py
+++ DebtDeficitSurplusApp.py
@@ -94,7 +94,7 @@ if viewType == "President":
 
     # # shows president info
     st.markdown(
-    f"<h1 style='text-align: center;'>{president} was in office from {startYear} to {endYear}.</h1>", 
+    f"<h3 style='text-align: center;'>{president} was in office from {startYear} to {endYear}.</h3>", 
     unsafe_allow_html=True
 )
 
